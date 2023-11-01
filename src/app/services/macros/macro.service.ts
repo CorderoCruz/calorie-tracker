5e3b2e0b1bfd6d30f04a07f99ce82497c463e377
Cruz Cordero Leon <105164264+CorderoCruz@users.noreply.github.com>
2023-11-01 15:53:36 -0400

    correcting creating entry and adding meal food

--- src/app/services/macros/macro.service.ts
+++ src/app/services/macros/macro.service.ts
@@ -1,16 +1,22 @@
-import { Injectable, Signal, computed, inject, signal } from "@angular/core";
-import { Entry, Macros } from "@interfaces";
-import { EntryService } from "../entry/entry-service.service";
-import { Utils } from "src/app/utils/utils";
-import { HttpClient } from "@angular/common/http";
-import { Observable, tap } from "rxjs";
-import { environment } from "src/environments/environment";
+import { Injectable, Signal, computed, inject, signal } from '@angular/core';
+import { Entry, Macros } from '@interfaces';
+import { EntryService } from '../entry/entry-service.service';
+import { Utils } from 'src/app/utils/utils';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export type HTTPResponse = {
+  status: number;
+  data: any;
+  message: string;
+};
 
 @Injectable({
-  providedIn: "root",
+  providedIn: 'root',
 })
 export class MacroService {
-  private readonly url: string = environment["apiUrl"];
+  private readonly url: string = environment['apiUrl'];
   hasLandedOnPage = signal<boolean>(false);
 
   private http = inject<HttpClient>(HttpClient);
@@ -20,7 +26,7 @@ export class MacroService {
   private fat = signal<number>(0);
   private carbs = signal<number>(0);
   private protein = signal<number>(0);
-  private date = signal<string>("");
+  private date = signal<string>('');
 
   public macrosLoading = signal<boolean>(false);
 
@@ -33,8 +39,8 @@ export class MacroService {
   }));
 
   public getMacrosFromDB(date: string) {
-    return this.http.get(`${this.url}/macros/date`, { params: { date } }).pipe(
-      tap((data: any) => {
+    return this.http.get<HTTPResponse>(`${this.url}/macros/date`, { params: { date } }).pipe(
+      tap(({ data }) => {
         if (!data?.data) {
           this.calories.set(0);
           this.fat.set(0);
@@ -42,8 +48,7 @@ export class MacroService {
           this.protein.set(0);
           return;
         }
-        const { calories, carbs, protein, fat, date } = data.data;
-        console.log(data.data);
+        const { calories, carbs, protein, fat, date } = data;
         this.calories.set(calories);
         this.fat.set(fat);
         this.carbs.set(carbs);
@@ -54,17 +59,13 @@ export class MacroService {
   }
 
   public updateMacros(weight: number, foodName: string, date: string) {
-    const food: Entry | undefined = this.entryService
-      .foodEntries()
-      .find((entry) => entry.name === foodName);
+    const food: Entry | undefined = this.entryService.foodEntries().find((entry) => entry.name === foodName);
     if (!food) {
       return;
     }
 
     const calculatedMacros: Partial<Macros> =
-      food["servingMeasurement"] === "one"
-        ? Utils.calculateMacros(1, food)
-        : Utils.calculateMacros(weight, food);
+      food['servingMeasurement'] === 'one' ? Utils.calculateMacros(1, food) : Utils.calculateMacros(weight, food);
     return this.updateMacrosToDB({
       ...calculatedMacros,
       date,
